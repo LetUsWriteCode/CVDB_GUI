5f7cf8e97d650231b13089bd201489eaba7f98b1
LetUsWriteCode <mark_wongchurdkwan@hotmail.com>
2018-10-10 23:31:00 +0100

    Added the server class for testing server connectivity

--- Classes.py
+++ Classes.py
@@ -3,13 +3,32 @@ import os
 
 #driver = '{ODBC Driver 17 for SQL Server}'
 
+class server(object):
+	def __init__ (self,server):
+		"""
+		Server based operations
+		"""
+		self.server = server
+
+	def ping_server(self):
+		"""Pings the server name and tracks a response"""
+
+		response = os.system("ping -c 1 " + self.server)
+		if response == 0:
+			message = '{} is reachable'.format(self.server)
+		else:
+			message = '{} is not reachable'.format(self.server)
+		return print(message)
+
+
 class connectDB(object):
-	def __init__ (self, server, database, username, password, driver='ODBC Driver 13 for SQL Server'):
+	def __init__ (self, server, database, username, password, driver='{ODBC Driver 17 for SQL Server}'):
 		"""
 		Attempts connection to the specified server and database using username and password
 		"""	
+
 #		connString = 'Driver={};Server={};Database={};Trusted_Connection=yes;'.format(driver,server,database) #Windows Auth
-		connString = 'Driver={};Server={};Database={};UID={};PWD={};'.format(driver,server,database,username,password) #SQL Auth
+		connString = 'Driver={};Server={}Database={};UID={};PWD={};'.format(driver,server,database,username,password) #SQL Auth
 		connect = pyodbc.connect(connString)
 		conn = connect.cursor()
 		return conn
@@ -32,6 +51,6 @@ class employer(object):
 		"""
 		SQLCommand = 'EXEC dbo.uspAddEmployer {}, {}, {}'.format(CompanyName,Website,Description)
 
-		conn = connectDB(server='192.168.1.161,1433',database='CVDatabase',username='devtest',password='devtest')
+		conn = connectDB(server='192.168.1.161',database='CVDatabase',username='devtest',password='devtest')
 		conn.execute(SQLCommand)
 		conn.close()
--- testing.py
+++ testing.py
@@ -1,9 +1,13 @@
-from Classes import employer
+import os
+from Classes import employer, connectDB, server
 
 company = 'Google'
 website = 'http://www.google.com'
 desc = 'Do No Evil'
 
+
+ser = server('192.168.1.161')
+ser.ping_server()
 
 t = employer()
 t.addEmployer(company,website, desc)
