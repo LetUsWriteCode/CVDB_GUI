66930d93f40cdad8c75b5964b7c3bc24fb2c583a
LetUsWriteCode <mark_wongchurdkwan@hotmail.com>
2018-10-13 00:51:44 +0100

    New methods added and a bit of code consolidation. Actually working when running locally

--- Classes.py
+++ Classes.py
@@ -1,13 +1,10 @@
 import pyodbc
 import os
-
-#driver = '{ODBC Driver 17 for SQL Server}'
+from PIL import Image
 
 class server(object):
 	def __init__ (self,server):
-		"""
-		Server based operations
-		"""
+		"""Server based operations"""
 		self.server = server
 
 	def ping_server(self):
@@ -21,36 +18,83 @@ class server(object):
 		return print(message)
 
 
-class connectDB(object):
-	def __init__ (self, server, database, username, password, driver='{ODBC Driver 17 for SQL Server}'):
-		"""
-		Attempts connection to the specified server and database using username and password
-		"""	
-
-#		connString = 'Driver={};Server={};Database={};Trusted_Connection=yes;'.format(driver,server,database) #Windows Auth
-		connString = 'Driver={};Server={}Database={};UID={};PWD={};'.format(driver,server,database,username,password) #SQL Auth
-		connect = pyodbc.connect(connString)
+class database(object):
+	def __init__ (self, server, database, username, password, driver=None):
+		self.server = server
+		self.database = database
+		self.username = username
+		if driver == None:
+			self.driver = '{ODBC Driver 13 for SQL Server}'
+		else:
+			self.driver = driver
+		return None
+	def _openConnection (self):
+		"""Attempts connection to the specified server and database using username and password"""
+		connString = 'Driver={};Server={};Database={};Trusted_Connection=yes;'.format(self.driver,self.server,self.database) #Windows Auth
+#		connString = 'Driver={};Server={}Database={};UID={};PWD={};'.format(driver,server,database,username,password) #SQL Auth
+		connect = pyodbc.connect(connString, autocommit=True)
 		conn = connect.cursor()
 		return conn
+	def sendSQL(self, statement):
+		"""Sends the SQL Statement to the database. Doesn't return an output"""
+		c = self._openConnection()
+		c.execute(statement)
+		c.close()
 
 class employer(object):
 	def __init__ (self):
-		"""
-		Allows Employer operations to the database
-		"""
+		"""Allows Employer operations to the database"""
 
 	def searchEmployer(self, searchString):
-		"""
-		Searches the database for employer
-		and returns EmployerID or Not Found
-		"""
+		"""Searches the database for employer and returns EmployerID or Not Found"""
 
 	def addEmployer(self, CompanyName, Website=None, Description=None):
-		"""
-		Adds a new employer to the database
-		"""
-		SQLCommand = 'EXEC dbo.uspAddEmployer {}, {}, {}'.format(CompanyName,Website,Description)
+		"""Adds a new employer to the database"""
+		SQLCommand = "EXEC dbo.uspAddEmployer '{}', '{}', '{}'".format(CompanyName,Website,Description)
 
-		conn = connectDB(server='192.168.1.161',database='CVDatabase',username='devtest',password='devtest')
-		conn.execute(SQLCommand)
-		conn.close()
+		conn = database(server='DESKTOP-F16R844',database='CVDatabase',username='devtest',password='devtest')
+		conn.sendSQL(SQLCommand)
+	def addEmployerLogo(self,employerId,Image):
+		f = open(Image, 'rb')
+		ablob = f.read()
+		#ablob = pyodbc.Binary(ablob)
+		SQLCommand = 'INSERT INTO dbo.EmployerLogo (EmployerID, LogoBLOB) VALUES (?, ?)'.format(employerId, ablob)
+
+		print(SQLCommand)
+		conn = database(server='DESKTOP-F16R844',database='CVDatabase',username='devtest',password='devtest')
+		c = conn._openConnection()
+		c.execute(SQLCommand,employerId,pyodbc.Binary(ablob))
+		c.close()
+
+
+class subject(object):
+	def __init__ (self):
+		"""Allows Subject (person) operations to the database"""
+		return None
+	
+	def returnSubjects(self):
+		"""Returns all the subjects from the Subject table"""
+		SQLCommand = 'SELECT * FROM dbo.Subject'
+
+		conn = database(server='DESKTOP-F16R844',database='CVDatabase',username='devtest',password='devtest')
+		c = conn._openConnection()
+		for row in c.execute(SQLCommand):
+			print(row)
+		c.close()
+
+	def searchSubject(self, forename=None, surname=None, dob=None):
+		"""Searches for a subject by Forename, Lastname or DOB"""
+		if forename == None:
+			forename = '%'
+		if surname == None:
+			surname = '%'
+		if dob == None:
+			dob = '%'
+		
+		SQLCommand = "SELECT * FROM dbo.Subject WHERE Forename LIKE '{}'".format(forename)
+		
+		conn = database(server='DESKTOP-F16R844',database='CVDatabase',username='devtest',password='devtest')
+		c = conn._openConnection()
+		for row in c.execute(SQLCommand):
+			print(row)
+		c.close()
--- testing.py
+++ testing.py
@@ -1,13 +1,40 @@
 import os
-from Classes import employer, connectDB, server
+from Classes import employer, database, server, subject
+import pyodbc
 
 company = 'Google'
 website = 'http://www.google.com'
 desc = 'Do No Evil'
 
 
-ser = server('192.168.1.161')
-ser.ping_server()
+#ser = server('DESKTOP-F16R844')
+#ser.ping_server()
 
 t = employer()
-t.addEmployer(company,website, desc)
+#t.addEmployer(company,website, desc)
+t.addEmployerLogo(1,'C:\Pics\Google.jpg')
+
+#p = subject()
+#p.searchSubject(forename = 'Mark')
+
+"""
+class connectDB(object):
+	driver = '{ODBC Driver 13 for SQL Server}'
+	server = 'DESKTOP-F16R844'
+	db = 'CVDatabase'
+	username = 'devtest'
+	password = 'devtest'
+	def __init__(self):
+		print('opn')	
+	def opnCur(self):
+		connString = 'Driver={};Server={};Database={};Trusted_Connection=yes;'.format(self.driver,self.server,self.db)
+		#connString = 'Driver={};Server={}Database={};User Id={};Password={};'.format(driver,server,db,username,password) #SQL Auth
+		connect = pyodbc.connect(connString)
+		conn = connect.cursor()
+		return conn
+
+con = connectDB()
+c = con.opnCur()
+for row in c.execute('SELECT * FROM dbo.Subject'):
+	print(row)
+"""
